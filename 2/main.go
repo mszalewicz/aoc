865fd92ad62f5712c8379a65d5d4348f647b25e1
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-12-02 11:11:49 +0100

    Day 2 Part 2

--- 2/main.go
+++ 2/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
 // Times:
 //     - part 1: 514µs
-//     - part 2:
+//     - part 2: 655µs
 
 func main() {
 	content, err := os.ReadFile("input")
@@ -22,60 +22,68 @@ func main() {
 
 	lines := strings.Split(string(content), "\n")
 
+	reports := make([][]int, 0)
 	numberOfSafeReports := 0
 
-	start := time.Now()
+	convertReports(lines, &reports)
 
-	for _, line := range lines {
-		numbersText := strings.Fields(line)
-
-		increasing := true
-		decreasing := true
-		safe := true
-
-		for i := 0; i < len(numbersText)-1; i++ {
-			var number1, number2 int
-			var err error
-
-			if number1, err = strconv.Atoi(numbersText[i]); err != nil {
-				log.Fatal(err)
-			}
-
-			if number2, err = strconv.Atoi(numbersText[i+1]); err != nil {
-				log.Fatal(err)
-			}
-
-			switch {
-			case number1 < number2:
-				if number2-number1 > 3 {
-					safe = false
-					break
-				}
-				decreasing = false
-			case number1 > number2:
-				if number1-number2 > 3 {
-					safe = false
-					break
-				}
-				increasing = false
-			default:
-				safe = false
-				break
-			}
-		}
-
-		if !increasing && !decreasing {
-			safe = false
-		}
-
-		if safe {
+	for _, report := range reports {
+		if isSafeReport(report) || isSafeWithOneRemoval(report) {
 			numberOfSafeReports++
 		}
-
 	}
 
-	end := time.Since(start)
-	fmt.Println(end)
+	fmt.Println(numberOfSafeReports)
+}
 
-	fmt.Println("Part 1:", numberOfSafeReports)
+func convertReports(lines []string, reports *[][]int) {
+	for _, line := range lines {
+		report := make([]int, 0)
+
+		for _, text := range strings.Fields(line) {
+			value, err := strconv.Atoi(text)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			report = append(report, value)
+		}
+
+		*reports = append(*reports, report)
+	}
+}
+
+func isSafeReport(report []int) bool {
+	increasing := true
+	decreasing := true
+
+	for i := 0; i < len(report)-1; i++ {
+		diff := math.Abs(float64(report[i]) - float64(report[i+1]))
+
+		switch {
+		case diff < 1 || diff > 3:
+			return false
+		case report[i] < report[i+1]:
+			decreasing = false
+		case report[i] > report[i+1]:
+			increasing = false
+		}
+	}
+
+	return increasing || decreasing
+}
+
+func isSafeWithOneRemoval(report []int) bool {
+	for i := 0; i < len(report); i++ {
+
+		modifiedReport := make([]int, 0)
+		modifiedReport = append(modifiedReport, report[:i]...)
+		modifiedReport = append(modifiedReport, report[i+1:]...)
+
+		if isSafeReport(modifiedReport) {
+			return true
+		}
+	}
+	return false
 }
