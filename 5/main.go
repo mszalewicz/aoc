2d1a52cf8e80f65f736e01f2ad2fb66ca4dc3370
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-12-05 08:57:11 +0100

    Day 5 Part 2

--- 5/main.go
+++ 5/main.go
@@ -66,8 +66,10 @@ func main() {
 	}
 
 	result1 := part1(rules, entries)
+	result2 := part2(rules, entries)
 
 	fmt.Println("Part1:", result1)
+	fmt.Println("Part2:", result2)
 }
 
 func part1(rules map[int][]int, entries [][]int) int {
@@ -91,4 +93,57 @@ EntriesLoop:
 	}
 
 	return result
+}
+
+func part2(rules map[int][]int, entries [][]int) int {
+	result := 0
+
+EntriesLoop:
+	for _, entry := range entries {
+		visited := []int{}
+		for _, value := range entry {
+			for _, rule := range rules[value] {
+
+				// if entry has incorrect order:
+				if slices.Contains(visited, rule) {
+					// reorder to correct form and add middle value to result:
+					reorderEntry(&entry, rules)
+					result += entry[len(entry)/2]
+
+					continue EntriesLoop
+				}
+
+			}
+			visited = append(visited, value)
+		}
+	}
+
+	return result
+}
+
+func reorderEntry(entry *[]int, rules map[int][]int) {
+	for {
+		swapped := false
+		for i := 0; i < len(*entry)-1; i++ {
+			if shouldComeBefore((*entry)[i], (*entry)[i+1], rules) {
+				(*entry)[i], (*entry)[i+1] = (*entry)[i+1], (*entry)[i]
+				swapped = true
+			}
+		}
+
+		if !swapped {
+			break
+		}
+	}
+}
+
+func shouldComeBefore(currentValue int, nextValue int, rulesMap map[int][]int) bool {
+	rules := rulesMap[currentValue]
+
+	for _, rule := range rules {
+		if rule == nextValue {
+			return true
+		}
+	}
+	return false
 }
